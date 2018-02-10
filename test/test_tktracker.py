b7181df217e608caa7d817595242e9333df945e7
ooklah <ooklah@gmail.com>
2018-02-09 23:52:53 -0800

    added elapsed_time() elapsed time will return the time in seconds of all the laps. Fixed the stop_flag not properly being set.
    Updated and wrote new tests.

--- test/test_tktracker.py
+++ test/test_tktracker.py
@@ -10,6 +10,11 @@ import time
 from timekeeper.tktracker import TKTracker, TrackerStartError, TrackerStopError
 
 
+def s(c=1):
+    """Shorthand sleep timer."""
+    time.sleep(c)
+
+
 class TestTKTracker(unittest.TestCase):
 
     def setUp(self):
@@ -24,7 +29,8 @@ class TestTKTracker(unittest.TestCase):
         is called twice.
         """
         self.tk.start()
-        self.assertRaises(TrackerStartError, self.tk.start())
+        with self.assertRaises(TrackerStartError):
+            self.tk.start()
 
     def test_double_stop_fail(self):
         """
@@ -32,18 +38,81 @@ class TestTKTracker(unittest.TestCase):
         """
         self.tk.start()
         self.tk.stop()
-        self.assertRaises(TrackerStopError, self.tk.stop())
+        with self.assertRaises(TrackerStopError):
+            self.tk.stop()
 
     def test_stop_without_start(self):
         """Test that the tracker fails if stop is calle before start."""
-        self.assertRaises(TrackerStopError, self.tk.stop())
+        with self.assertRaises(TrackerStopError):
+            self.tk.stop()
 
+    # @unittest.SkipTest
     def test_start_stop(self):
         """Test that starts and stops the tracker without issue."""
         self.tk.start()
-        time.sleep(1)
+        s()
         self.tk.stop()
         self.assertEqual(len(self.tk._total_laps[0]), 2, self.tk._total_laps)
+
+    # @unittest.SkipTest
+    def test_start_restart(self):
+        """Test Starting and then starting the same tracker again."""
+        self.tk.start()
+        s()
+        with self.assertRaises(TrackerStartError):
+            self.tk.start()
+
+    # @unittest.SkipTest
+    def test_elapsed_time(self):
+        """
+        Test the elapsed time property to ensure it returns the correct
+        value on one cyle.
+        """
+        self.tk.start()
+        s(5)
+        self.tk.stop()
+        # Round to the nearest whole number for now.
+        self.assertEquals(int(self.tk.elapsed_time), 5)
+
+    # @unittest.SkipTest
+    def test_elapsed_time_laps(self):
+        """
+        Test the elapsed time with multiple time lapses.
+        """
+        for i in range(5):
+            self.tk.start()
+            s()
+            self.tk.stop()
+        
+        # Round to the nearest whole number.
+        self.assertEqual(int(self.tk.elapsed_time), 5)
+
+    # @unittest.SkipTest
+    def test_no_lap_elapsed_without_stop(self):
+        """
+        Test getting the elapsed time before the tracker has been 
+        stopped on the first lap."""
+        self.tk.start()
+        s()
+        self.assertEqual(int(self.tk.elapsed_time), 1)
+        s()
+        self.tk.stop()
+        self.assertEqual(int(self.tk.elapsed_time), 2)
+
+    def test_lap_counter_one(self):
+        """Test that the lap counter returns one lap on first lap."""
+        self.tk.start()
+        self.assertEqual(self.tk.laps, 1)
+
+    def test_lap_counter_two(self):
+        """Test lap counter on 1.5 laps"""
+        self.tk.start()
+        s()
+        self.tk.stop()
+        s()
+        self.tk.start()
+        s()
+        self.assertEqual(self.tk.laps, 2)
 
 
 if __name__ == '__main__':
--- tktracker.py
+++ tktracker.py
@@ -21,15 +21,44 @@ class TKTracker(object):
         self._lap = []
         self._total_laps = []
         self.results = {}
-        self.stop_flag = False
+        self.stop_flag = True
 
     @property
     def id(self):
         return self._id
 
+    @property
+    def elapsed_time(self):
+        """Return the total amount of time that has elapsed so far."""
+        total_time = 0
+        # Get the total time from previous laps
+        for lap in self._total_laps:
+            if len(lap) == 2:
+                total_time = total_time + (lap[1] - lap[0])
+            # I don't think this should ever be a case.
+            if len(lap) == 1:
+                tmp = list(lap)
+                tmp.append(time.time())
+                total_time = total_time + (tmp[1] - tmp[0])
+
+        # Get the time from the current lap if we're in one.
+        if len(self._lap):
+            total_time = total_time + (time.time() - self._lap[0])
+
+        return total_time
+
+    @property
+    def laps(self):
+        # Get previous laps
+        count = len(self._total_laps)
+        # Get the current lap
+        if len(self._lap) == 1:
+            count += 1
+        return count
+
     def start(self):
         """Start the tracker."""
-        if not self.stop_flag:
+        if self.stop_flag:
             self._lap.append(time.time())
             self.stop_flag = False
         else:
@@ -41,5 +70,6 @@ class TKTracker(object):
             self._lap.append(time.time())
             self._total_laps.append(self._lap)
             self._lap = []
+            self.stop_flag = True
         else:
             raise TrackerStopError("Already stopped.")
